c2e1baa324d44885add700fab483801ef981beca
kimhyerin0909 <khyerin0909@gmail.com>
2022-12-22 16:59:07 +0900

    Add - color palette

---
+++ src/constants/colors.ts
@@ -0,0 +1,30 @@
+export const KKM_GRADIENT =
+  "linear-gradient(0deg, #726FD6 0.01%, #96A2EA 51.88%, #A1C0E6 100%);";
+
+export const BLUE_400 = "#595FFF";
+export const BLUE_350 = "#6573F5";
+export const BLUE_300 = "#7886FF";
+export const BLUE_250 = "#9AA4FF";
+export const BLUE_200 = "#CBD1FF";
+export const BLUE_150 = "#DFE6FF";
+export const BLUE_100 = "#EBF0FF";
+export const BLUE_50 = "#F6F8FF";
+
+export const GRAY_400 = "#404040";
+export const GRAY_350 = "#797979";
+export const GRAY_300 = "#828282";
+export const GRAY_250 = "#8E8E8F";
+export const GRAY_200 = "#BEBEBE";
+export const GRAY_150 = "#D9D9D9";
+export const GRAY_100 = "#E9E9EA";
+export const GRAY_50 = "#F5F5F5";
+
+export const PURPLE = "#9747FF";
+export const INDIGO = "#242F9B";
+export const BLUE = "#3E41FF";
+export const SKYBLUE = "#A1C0E6";
+export const LIGHTBLUE = "#DCE8FFCC";
+
+export const RED = "#D34646";
+export const YELLOW = "#FFE147";
+export const KORAL = "#FF5160";
