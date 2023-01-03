c3ae0d803049ab4b84e2beac25d03e151f7a0b99
kimhyerin0909 <khyerin0909@gmail.com>
2023-01-03 11:53:09 +0900

    Feat - route

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,21 @@
 import React from "react";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import { Footer } from "./components/Footer";
+import { Header } from "./components/Header";
+import { Layout } from "./components/Layout";
+import { Home } from "./pages/home/Home";
 function App() {
-  return <div className="App"></div>;
+  return (
+    <BrowserRouter>
+      <Header />
+      <Layout>
+        <Routes>
+          <Route path="/" element={<Home />} />
+        </Routes>
+      </Layout>
+      <Footer />
+    </BrowserRouter>
+  );
 }
 
 export default App;
