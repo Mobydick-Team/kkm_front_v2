9d3294fc53f8259c4871e1fd331160664f67d981
kimhyerin0909 <khyerin0909@gmail.com>
2023-03-23 14:11:26 +0900

    Set - recoil root, queryclientprovide 세팅

--- src/App.tsx
+++ src/App.tsx
@@ -1,21 +1,30 @@
 import React from "react";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
-import { Footer } from "./components/shared/Footer";
-import { Header } from "./components/shared/Header";
+import { Footer } from "./components/shared/footer/Footer";
+import { Header } from "./components/shared/header/Header";
 import { Layout } from "./components/layout/Layout";
 import { Home } from "./pages/home/Home";
-function App() {
+import { RecoilRoot } from "recoil";
+import { QueryClient, QueryClientProvider } from "react-query";
+
+const App = () => {
+  const queryClient = new QueryClient();
+
   return (
-    <BrowserRouter>
-      <Header />
-      <Layout>
-        <Routes>
-          <Route path="/" element={<Home />} />
-        </Routes>
-      </Layout>
-      <Footer />
-    </BrowserRouter>
+    <RecoilRoot>
+      <QueryClientProvider client={queryClient}>
+        <BrowserRouter>
+          <Header />
+          <Layout>
+            <Routes>
+              <Route path="/" element={<Home />} />
+            </Routes>
+          </Layout>
+          <Footer />
+        </BrowserRouter>
+      </QueryClientProvider>
+    </RecoilRoot>
   );
-}
+};
 
 export default App;
